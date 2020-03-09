a05dfbe687f64a2e35531a04799b1551360cee50
Abhijeet Pansari <abhijeetpansari123@gmail.com>
2020-03-09 14:18:52 +0530

    initial commit with few simple progs

---
+++ Logical.java
@@ -0,0 +1,15 @@
+/**
+ * Logical
+ */
+public class Logical {
+
+    public static void main(String[] args) {
+        boolean a= true;
+        boolean b= false;
+        boolean c= true;
+        System.out.println("Logical &&: " + (a&&b));
+        System.out.println("Logical ||: " + (a||b));
+        System.out.println("Logical &&: " + (a&&c));
+        System.out.println("Logical ||: " + (a||c));
+    }
+}
