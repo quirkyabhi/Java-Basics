fae9f37e56d3cc41480e134ac1b05995f7554276
Abhijeet Pansari <abhijeetpansari123@gmail.com>
2020-03-09 17:58:51 +0530

    new files

---
+++ For.java
@@ -0,0 +1,17 @@
+/**
+ * For
+ */
+public class For {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("i= "+ i);
+            
+        }
+        int[] arr={10,20,30};
+        for (int n : arr) {
+            System.out.println(n);
+            
+        }
+    }
+}
---
+++ While.java
@@ -0,0 +1,13 @@
+/**
+ * While
+ */
+public class While {
+
+    public static void main(String[] args) {
+        int i=10;
+        while (i>0) {
+            System.out.println("i= "+ i);
+            --i;
+        }
+    }
+}
