d14180b8ed2a4a193228a9913c54bfc244bccfae
Abhijeet Pansari <abhijeetpansari123@gmail.com>
2020-03-13 19:34:21 +0530

    OOPS started

---
+++ Accounts.java
@@ -0,0 +1,9 @@
+/**
+ * Accounts
+ */
+public class Accounts {
+    float BasicInterest= 2.0f;
+    public void WelcomeUser(String user) {
+        System.out.println(" Welcome : "+ user);
+    }
+}
