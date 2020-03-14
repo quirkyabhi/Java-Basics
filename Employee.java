fd3ef699a3e084f06df429503b3bedba63fe287c
Abhijeet Pansari <abhijeetpansari123@gmail.com>
2020-03-14 19:02:25 +0530

    Constructor prog added

---
+++ Employee.java
@@ -0,0 +1,12 @@
+/**
+ * Constructor
+ */
+public class Employee {
+    int empCode;
+    int deptCode;
+    String firstName;
+    String lastName;
+    Employee(){
+        this.deptCode=101;
+    }
+}
---
+++ EmployeeDetails.java
@@ -0,0 +1,14 @@
+/**
+ * EmployeeDetails
+ */
+public class EmployeeDetails {
+
+    public static void main(String[] args) {
+        Employee emp= new Employee();
+        emp.firstName="Nishant";
+        emp.lastName="Mishra";
+
+        System.out.println("Employee Details : \n"+ emp.empCode+"\n" +emp.deptCode+"\n" + emp.firstName+"\n" + emp.lastName);
+
+    }
+}
