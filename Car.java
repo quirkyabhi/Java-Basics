ae79a3c202e3bbfe488eac9365c14acde0c865cf
Abhijeet Pansari <abhijeetpansari123@gmail.com>
2020-03-14 18:30:00 +0530

    Some basic progs like Factorial, prime , fibonacci

---
+++ Car.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * Car
+ */
+public class Car {
+    String color;
+    int speed;
+    String model;
+    int price;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Car obj= new Car();
+        System.out.println("Enter model");
+        obj.model= sc.next();
+        System.out.println("Enter speed");
+        obj.speed= sc.nextInt();
+        System.out.println("Enter color");
+        obj.color= sc.next();    
+        System.out.println("Enter price");
+        obj.price= sc.nextInt();
+
+        System.out.println("model: "+obj.model+ " \nspeed: "+ obj.speed+ " \ncolor: "+ obj.color+ " \nPrice: "+ obj.price);
+
+    }
+    
+
+    
+
+    // void getColor(){
+
+    // }
+    // void getSpeed(){
+
+    // }
+    // void getCost(){
+
+    // }
+}
