1be28daea8e5c4e3bbb98e52170d4ae641c1fe37
Abhijeet Pansari <abhijeetpansari123@gmail.com>
2020-03-16 18:56:27 +0530

    small changes

--- ConstructorChaining.java
+++ ConstructorChaining.java
@@ -1,20 +1,51 @@
 /**
  * ConstructorChaining
  */
-public class ConstructorChaining {
+// public class ConstructorChaining {
+//     int x;
+//     public  ConstructorChaining() {
+//         System.out.println("Default");
+        
+//     }
+
+//     public  ConstructorChaining(int n) {
+//         this();
+//         this.x=n;
+//     }
+//     public static void main(String[] args) {
+//         ConstructorChaining obj= new ConstructorChaining(10);
+//         System.out.println("x: "+obj.x); 
+        
+//     }
+// }
+
+/**
+ * Base
+ */
+class Base {
+
+    public  Base() {
+        System.out.println("Base Default");
+        
+    }
+}
+
+public class ConstructorChaining extends Base{
     int x;
     public  ConstructorChaining() {
-        System.out.println("Default");
+        // this(10);
+        super();
+        System.out.println("Child Default");
         
     }
 
-    public  ConstructorChaining(int n) {
-        this();
-        this.x=n;
-    }
+    // public  ConstructorChaining(int n) {
+        
+    //     this.x=n;
+    // }
     public static void main(String[] args) {
-        ConstructorChaining obj= new ConstructorChaining(10);
-        System.out.println("x: "+obj.x); 
+        ConstructorChaining obj= new ConstructorChaining();
+        // System.out.println("x: "+obj.x); 
         
     }
 }
